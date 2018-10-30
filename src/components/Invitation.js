056ca36b2263a3f9119537661fc2dd987f35fdc3
sondos alansi <sondos.alansi@yahoo.com>
2018-10-30 19:22:03 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -3,15 +3,16 @@ export default class Invitation extends React.Component {
   render() {
     const childrenWithWrapperDiv = React.Children.map(this.props.children, child => {
       return (
-        <div className="some-component-special-class">{child}</div> 
+        <h1>Youve been invited!</h1>
+        <div className="some-component-special-class">{child}</div>
       );
     });
- 
+
     return (
       <div className="some-component">
         <p>This component has {React.Children.count(this.props.children)} children.</p>
-        {childrenWithWrapperDiv}        
-      </div>      
+        {childrenWithWrapperDiv}
+      </div>
     );
   }
 }
