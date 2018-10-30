dbe501622e2aedf4dced5ea6fa1485cedef7dbe6
sondos alansi <sondos.alansi@yahoo.com>
2018-10-30 19:30:04 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,14 +1,14 @@
 // Code Invitation Component Here
 export default class Invitation extends React.Component {
   render() {
-    const childrenWithWrapperDiv = React.Children.map(this.props.children, child => {
+    
       return (
         <h1>Youve been invited!</h1>
         <h2>{this.props.children}</h2>
-      
-      );
-    });
 
-  
+      );
+    );
+
+
   }
 }
