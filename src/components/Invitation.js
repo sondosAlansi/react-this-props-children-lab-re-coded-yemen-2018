b3d4e6d4e71f6bb0ae7024f871398b72f465d6d5
sondos alansi <sondos.alansi@yahoo.com>
2018-10-30 19:30:39 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -3,9 +3,10 @@ export default class Invitation extends React.Component {
   render() {
 
       return (
+        <div>
         <h1>Youve been invited!</h1>
         <h2>{this.props.children}</h2>
-
+        </div>
       );
     );
 
